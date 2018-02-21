8af90b277720d433cdd8bbc77207fa0e9a2f47c8
CristianFM <cristianfleitasmoreno@gmail.com>
2018-02-21 08:45:29 +0000

    Inicio Examen commit

--- ejercicios/ejercicio1.php
+++ ejercicios/ejercicio1.php
@@ -1,7 +1,35 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jmaur_000
- * Date: 20/02/2018
- * Time: 17:55
- */
+
+$array = file("../ficheros/comunidades.txt");
+
+$arrayMostrado = [];
+
+foreach($array as $a){
+    $aux = explode("%", $a);
+    if(!isset($arrayMostrado[$aux[1]])){
+        $arrayMostrado[$aux[1]] = [];
+    }
+}
+var_dump($arrayMostrado);
+
+?>
+<html>
+<head>
+
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Listado de Provincias</th>
+            <th>Poblacion</th>
+        </tr>
+        <?php
+
+        foreach($arrayMostrado as $k){
+
+//            echo "<tr>" .$k. "</tr>";
+        }
+        ?>
+    </table>
+</body>
+</html>
