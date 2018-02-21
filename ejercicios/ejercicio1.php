610832c4d626e33fa5b199eae4caa39e03a77d0f
CristianFM <cristianfleitasmoreno@gmail.com>
2018-02-21 09:06:18 +0000

    Tercer  commit

--- ejercicios/ejercicio1.php
+++ ejercicios/ejercicio1.php
@@ -1,16 +1,24 @@
 <?php
 
-$array = file("../ficheros/comunidades.txt");
+$arrayFileProvincias = file("../ficheros/comunidades.txt");
+$arrayFileMunicipios = file("../ficheros/municipios.txt");
 
-$arrayMostrado = [];
+$arrayProvincias = [];
 
-foreach($array as $a){
-    $aux = explode("%", $a);
-    if(!isset($arrayMostrado[$aux[1]])){
-        $arrayMostrado[$aux[1]] = [];
+foreach($arrayFileProvincias as $a){
+    $provincia = explode("%", $a);
+    if(!isset($arrayProvincias[$provincia[0]])){
+        $arrayProvincias[$provincia[0]] = [];
     }
+    array_push($arrayProvincias[$provincia[0]], $provincia[1]);
 }
-var_dump($arrayMostrado);
+//var_dump($arrayProvincias);
+foreach($arrayFileMunicipios as $b){
+    $municipio = explode("%", $b);
+
+   var_dump($municipio);echo"<br/>";
+}
+
 
 ?>
 <html>
@@ -25,9 +33,8 @@ var_dump($arrayMostrado);
         </tr>
         <?php
 
-        foreach($arrayMostrado as $k){
-
-//            echo "<tr>" .$k. "</tr>";
+        foreach($arrayProvincias as $k => $v){
+//              echo  "<tr><td>".$v[0]."</td><tr>";
         }
         ?>
     </table>
