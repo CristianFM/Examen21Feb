992ee3a9a9d6fa95bbd07a52555d4b7d8bc4b708
CristianFM <cristianfleitasmoreno@gmail.com>
2018-02-21 08:16:54 +0000

    Primer commit

---
+++ ejercicios/ejercicio1.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jmaur_000
+ * Date: 20/02/2018
+ * Time: 17:55
+ */
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+Examen de PHP (21 de febrero de 2018 )
+
+</body>
+</html>
